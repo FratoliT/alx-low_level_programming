a69c62592d4194767e0a30f94ce05478defa97c5
fratolit <fratoli.tesfa@outlook.com>
2023-03-18 13:03:50 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - using puts function to print a line 
- *
- * Return: Always 0 (Success)
+ * main -using puts function to print a line
+ * Return: 0 (Success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
