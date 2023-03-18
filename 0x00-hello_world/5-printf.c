dad76af6099af9f7169ecd116980af396d0d9881
fratolit <fratoli.tesfa@outlook.com>
2023-03-18 14:09:40 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-* main - Using printf function to print a line
+* main -Using printf function to print a line
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-Printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
+
