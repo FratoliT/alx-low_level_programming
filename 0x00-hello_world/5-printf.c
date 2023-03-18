e491ab21394e7dfb6c394a35153b3151d9b1077b
fratolit <fratoli.tesfa@outlook.com>
2023-03-18 13:34:44 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - Using printf function to print a line
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+Printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
