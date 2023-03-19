d9bf69995f818a00adb26b69acb785a042ea4a1d
fratolit <fratoli.tesfa@outlook.com>
2023-03-19 12:47:39 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,26 +4,24 @@
 
 /**
 *  main -a function that checks if the number is posetive, negative or zero
-* return: Always 0 (sucess)
+* Return: Always 0 (sucess)
 */
-
 int main(void)
 {
 int n;
-
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf ("%d is positive\n", n);
+printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf ("%d is zero\n", n);
+printf("%d is zero\n", n);
 }
 else
 {
-printf ("%d is negative\n", n);
+printf("%d is negative\n", n);
 }
-	return (0);
+return (0);
 }
