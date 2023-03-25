3a9c7b825da1e026e39a4851227e34673d456674
fratolit <fratoli.tesfa@outlook.com>
2023-03-25 15:18:59 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,19 +1,18 @@
 #include <stdlib.h>
+
 #include <time.h>
+
 #include <stdio.h>
 
-/** more headers goes there 
-* betty style doc for function main goes there
-*/
+/ main - prints betty a number /
 
 int main(void)
 {
-	int n; int lastd;
+	int n, lastd;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	lastd = n % 10;
-	
 	if (lastd > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
