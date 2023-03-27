6784b043bda11a23d639211c0d77c986e28e91a2
fratolit <fratoli.tesfa@outlook.com>
2023-03-27 14:16:54 -0700

    A good engineer thinks in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,12 +8,11 @@
 
 void print_rev(char *s)
 {
-int = a;
-a = 0;
+int i = 0;
 
-while (s[a] != '\0')
+while (s[i] != '\0')
 i++;
-for (a = a - 1; a >= 0; a--)
-_putchar(s[a]);
+for (i = i - 1; i >= 0; i--)
+_putchar(s[i]);
 _putchar('\n');
 }
