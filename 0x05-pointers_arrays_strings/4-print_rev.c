1b36798ee9ff29d02888b47445c811a41c476178
fratolit <fratoli.tesfa@outlook.com>
2023-03-27 14:10:24 -0700

    A good engineer thinks in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print _rev - A function that prints a string, in reverse,
+ * followed by a new line.
+ * @s: string to be printed
+ */
+
+void print_rev(char *s)
+{
+int = a;
+a = 0;
+
+while (s[a] != '\0')
+i++;
+for (a = a - 1; a >= 0; a--)
+_putchar(s[a]);
+_putchar('\n');
+}
