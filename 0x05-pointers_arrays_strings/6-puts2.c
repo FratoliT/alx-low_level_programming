fabc4291c0232582d77196c35d29aa9a55dab446
fratolit <fratoli.tesfa@outlook.com>
2023-03-28 13:26:08 -0700

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,19 @@
 #include "main.h"
 
 /**
- *puts2 - prints prints one character out of two
- *@str - input 
- *Return:print
+ * puts2 - Function prints one character out of two
+ * starting with the first one
+ * @str - input
+ * Return:print
  */
 
 void puts2(char *str)
 {
 int i = 0;
+int longi = 0;
+char *y = 0;
+int 0;
+
 for (; str[i] != '\0'; i++)
 {
 if ((i % 2) == 0)
@@ -17,4 +22,5 @@ else
 continue;
 }
 _putchar('\n');
+
 }
