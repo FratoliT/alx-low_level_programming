e3c0e8973ad1624b82f182a2859cb23cddda05c2
fratolit <fratoli.tesfa@outlook.com>
2023-03-28 13:11:46 -0700

    A good engineer thinks in reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,23 +3,23 @@
 /**
  * rev_setting - reverse a string
  * @s: input
- * return:string in riverse
+ * Return: string in riverse
  */
 
 void rev_string(char *s)
 {
-	char rv = s[0];
-	int c = 0;
-	int i;
+char rev = s[0];
+int counter = 0;
+int i;
 
-	while (s[c] != '\0')
-	c++;
-	for (i = 0; i < c; i++)
+while (s[counter] != '\0')
+counter++;
+for (i = 0; i < counter; i++)
 {
-	c--;
-	rv = s[i];
-	s[i] = s[c];
-	s[c] = rv;
+counter--;
+rev = s[i];
+s[i] = s[counter];
+s[counter] = rev;
 }
 
 }
