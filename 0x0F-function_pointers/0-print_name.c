3a75c91b9e713dad269953fc1c21c37a5a81102d
fratolit <fratoli.tesfa@outlook.com>
2023-04-19 13:29:38 -0700

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "functions_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - prints name using pointers to function
+ * @name: string to add
+ * @f: pointer to function
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *));
+{
+	while (name ==NULL || f == NULL)
+		return;
+	f(name);
+}
