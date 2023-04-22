e435b95e2371671847acdd8d6d1b431bceb928ae
fratolit <fratoli.tesfa@outlook.com>
2023-04-22 11:00:16 -0700

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- *
- *
- *
+ * struct dog - a dog's basic info
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  */
 struct dog
 {
@@ -13,8 +14,9 @@ float age;
 char *owner;
 
 };
-
-
+/**
+ * dog_t - typedef for struct dog
+ */
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
