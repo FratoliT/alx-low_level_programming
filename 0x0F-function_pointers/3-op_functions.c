db99df8b862325c5502db920c3f4b9f0b05bc5fe
fratolit <fratoli.tesfa@outlook.com>
2023-04-23 12:36:23 -0700

    A goal is not always meant to be reached

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+/**
+ * op_add - returns the addition of two numbers
+ * @a: The first number
+ * @b: The second number
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_mul - returns the multiplication of two numbers
+ * @a: The first number
+ * @b: The second number
+ */
+int op_mul(int a, int b)
+{
+	return (a * b)
+}
+/**
+ * op_sub - returns the substaction of two numbers
+ * @a: The first number
+ * @b: The second number
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_div - returns the multiplication of two numbers
+ * @a: The first number
+ * @b: The second number
+ */
+int op_div(int a, int b);
+{
+	return (a * b);
+}
+/**
+ * op_div - returns the remainder of two number
+ * @a: the first number
+ * @b: The second number
+ */
+int op_mod(int a, int b);
+{
+	return (a % b);
+}
