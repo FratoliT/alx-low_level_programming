9e2c710efc1f9511066e76a0fabea780b109b1c5
fratolit <fratoli.tesfa@outlook.com>
2023-05-03 01:22:16 -0700

    List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in the list
+ * @: first pointer
+ * Return: number of element
+ */
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
