fef0d0f639be97df79acbb8d9e9e5206ce8e3a41
fratolit <fratoli.tesfa@outlook.com>
2023-06-13 11:27:42 -0700

    Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 /**
 * create_array - create array of size assign char
-* @: size of array
+* @c: size of array
 * @size: size of array
 * Description: create array of size and assign char c
 * Return: pointer to array, NULL is fail
